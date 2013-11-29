bf4d88e7944358fc9abc42aba28b7ed860ff79f4
chaseoes <chase.oes@gmail.com>
2013-11-28 20:57:39 -0700

    Update plugin template.

---
+++ src/main/java/com/chaseoes/plugintemplate/_PluginTemplate.java
@@ -0,0 +1,21 @@
+package com.chaseoes.plugintemplate;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class _PluginTemplate extends JavaPlugin {
+	
+	public void onEnable() {
+	    
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+	public boolean onCommand(CommandSender cs, Command cmnd, String string, String[] strings) {
+		return true;
+	}
+
+}
