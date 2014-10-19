ff7056f202a91dc208949b89d93f52f924214674
chaseoes <chase@chaseoes.com>
2014-10-19 11:25:36 -0700

    New plugin template!

--- src/main/java/com/chaseoes/_plugintemplate/_PluginTemplate.java
+++ src/main/java/com/chaseoes/_plugintemplate/_PluginTemplate.java
@@ -1,17 +1,23 @@
-package me.chaseoes.plugintemplate;
+package com.chaseoes._plugintemplate;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class _PluginTemplate extends JavaPlugin {
+    
+    private static _PluginTemplate instance;
+    
+    public static _PluginTemplate getInstance() {
+        return instance;
+    }
 	
 	public void onEnable() {
-		
+	    instance = this;
 	}
 	
 	public void onDisable() {
-		
+		instance = null;
 	}
 	
 	public boolean onCommand(CommandSender cs, Command cmnd, String string, String[] strings) {
